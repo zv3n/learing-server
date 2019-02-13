67690b58dc707eab8e06720258482eb972561c19
zvenzven <zvenzuehlsdorf@googlemail.com>
2019-02-13 15:19:07 +0100

    Add Get

--- server.js
+++ server.js
@@ -1,6 +1,53 @@
 const express = require('express') // Hier wird Express als Libray geladen
 const app = express() //meint unseren Server, in dem wir App als Funktion aufrufen
 
+const data = {
+  cards: [
+    {
+      title: 'Foo',
+      content: 'lorem',
+      id: 1,
+      tags: ['html', 'css', 'java script'],
+    },
+    {
+      title: 'Bar',
+      content: 'lorem',
+      id: 2,
+      tags: ['html', 'css', 'java script'],
+    },
+    {
+      title: 'Dies',
+      content: 'lorem',
+      id: 3,
+      tags: ['html', 'css', 'java script'],
+    },
+    {
+      title: 'Das',
+      content: 'lorem',
+      id: 4,
+      tags: ['html', 'css', 'java script'],
+    },
+  ],
+}
+
+app.use(express.json())
+
+app.get('/cards', (req, res) => {
+  res.json(data)
+})
+
+app.post('/cards', (req, res) => {
+  const newCard = req.body
+  const isNameEqual = user => user.title === newCard.title
+  let card = data.cards.find(isNameEqual)
+  if (card) {
+    card = { ...card, ...newCard }
+  } else {
+    data.cards.push(newCard)
+  }
+  res.json(data)
+})
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server ready on port 3000')
 })
